ce48ca3817fdd706bc3c3187315bed051e711a8f
alvinjaymaique <148608688+alvinjaymaique@users.noreply.github.com>
2024-05-02 19:18:33 +0800

    Add files via upload

--- Vocalizer/VoiceCategorizer.py
+++ Vocalizer/VoiceCategorizer.py
@@ -1,11 +1,18 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models
 import os
 import librosa
+import json
+from tensorflow.keras import layers, models # type: ignore
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import load_model # type: ignore
+
+import pyaudio
+import wave
+import threading
 
 class VoiceClassifier:
+    # @tf.function(reduce_retracing=True)
     def __init__(self, data_dir='dataset', test_size=0.2, desired_shape=(128, 128), sr=22050, hop_length=512, n_mels=128):
         self.data_dir = data_dir
         self.test_size = test_size
@@ -89,12 +96,64 @@ class VoiceClassifier:
         preprocessed_audio = self.preprocess_audio(audio_file)
         predictions = self.model.predict(np.expand_dims(preprocessed_audio, axis=0))
         predicted_class_index = np.argmax(predictions)
+        # Check if predicted class index exists in self.labels
         predicted_class = self.labels[predicted_class_index]
-        print("Predicted class:", predicted_class)
+        # print("Predicted class:", predicted_class)
+        return predicted_class
+
+    def save_model(self, model_path, labels_path):
+        self.model.save(model_path)
+        with open(labels_path, 'w') as f:
+            json.dump(self.labels, f)
+
+    def load_model(self, model_path, labels_path):
+        self.model = load_model(model_path)
+        with open(labels_path, 'r') as f:
+            labels_dict = json.load(f)
+            self.labels = list(labels_dict.values())
+
+    def record(self, output_filename, record_seconds=60, sample_rate=44100, channels=1, audio_format=pyaudio.paInt16):
+        self.output_filename = output_filename
+        self.record_seconds = record_seconds
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.audio_format = audio_format
+        self.frames = []
+        self.recording = False
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(format=self.audio_format,
+                                       channels=self.channels,
+                                       rate=self.sample_rate,
+                                       input=True,
+                                       frames_per_buffer=1024)
+        
+    def start_recording(self):
+        self.recording = True
+        self.frames = []
+        print("Recording...")
+        threading.Thread(target=self._record).start()
+
+    def _record(self):
+        for _ in range(0, int(self.sample_rate / 1024 * self.record_seconds)):
+            if not self.recording:
+                break
+            data = self.stream.read(1024)
+            self.frames.append(data)
+        self.stop_recording()
+
+    def save_to_file(self):
+        wf = wave.open(self.output_filename, 'wb')
+        wf.setnchannels(self.channels)
+        wf.setsampwidth(self.audio.get_sample_size(self.audio_format))
+        wf.setframerate(self.sample_rate)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+
 
 # Usage example
 if __name__ == "__main__":
     voice_classifier = VoiceClassifier(data_dir='dataset')
-    voice_classifier.train_model()
+    # voice_classifier.train_model()
+    voice_classifier.load_model('model.keras', 'labels.json')
     audio_file = 'tenor_pavarotti.wav'
     voice_classifier.predict_audio(audio_file)
